9d39957c944618e808cce6605a822409bfdd1e1d
Tanushri goel <tanushri@lpu.in>
2025-05-01 21:08:28 +0530

    change

--- src/app/api/create-task/route.ts
+++ src/app/api/create-task/route.ts
@@ -1,44 +1,44 @@
-import dbConnect from "@/utils/dbConnect";
-import { getServerSession, User } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
-import { Task, UserModel } from "@/model/User.schema";
-import mongoose from "mongoose";
+// import dbConnect from "@/utils/dbConnect";
+// import { getServerSession, User } from "next-auth";
+// // import { authOptions } from "../auth/[...nextauth]/route";
+// import { Task, UserModel } from "@/model/User.schema";
+// import mongoose from "mongoose";
 
-export async function POST(req: Request) {
-  await dbConnect();
-  const session = await getServerSession(authOptions);
-  const user: User = session?.user as User;
-  const { title, description, status, priority } = await req.json();
-  try {
-    const userId = new mongoose.Types.ObjectId(user._id);
-    const currUser = await UserModel.findById(userId);
-    if (!currUser) {
-      return Response.json(
-        {
-          success: false,
-          message: "Can't find user",
-        },
-        { status: 401 }
-      );
-    }
-    const newTask = { title, description, status, priority };
-    currUser.tasks?.push(newTask as Task);
-    await currUser.save();
-    return Response.json(
-      {
-        success: true,
-        message: "New task created successfully",
-      },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.log("An unexpected error occurred");
-    return Response.json(
-      {
-        success: false,
-        message: "Error creating message",
-      },
-      { status: 401 }
-    );
-  }
-}
+// export async function POST(req: Request) {
+//   await dbConnect();
+//   const session = await getServerSession(authOptions);
+//   const user: User = session?.user as User;
+//   const { title, description, status, priority } = await req.json();
+//   try {
+//     const userId = new mongoose.Types.ObjectId(user._id);
+//     const currUser = await UserModel.findById(userId);
+//     if (!currUser) {
+//       return Response.json(
+//         {
+//           success: false,
+//           message: "Can't find user",
+//         },
+//         { status: 401 }
+//       );
+//     }
+//     const newTask = { title, description, status, priority };
+//     currUser.tasks?.push(newTask as Task);
+//     await currUser.save();
+//     return Response.json(
+//       {
+//         success: true,
+//         message: "New task created successfully",
+//       },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.log("An unexpected error occurred");
+//     return Response.json(
+//       {
+//         success: false,
+//         message: "Error creating message",
+//       },
+//       { status: 401 }
+//     );
+//   }
+// }
--- src/app/api/get-tasks/route.ts
+++ src/app/api/get-tasks/route.ts
@@ -1,54 +1,54 @@
-import dbConnect from "@/utils/dbConnect";
-import { getServerSession, User } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
-import { UserModel } from "@/model/User.schema";
-import mongoose from "mongoose";
+// import dbConnect from "@/utils/dbConnect";
+// import { getServerSession, User } from "next-auth";
+// // import { authOptions } from "../auth/[...nextauth]/route";
+// import { UserModel } from "@/model/User.schema";
+// import mongoose from "mongoose";
 
-export async function GET(req: Request) {
-  await dbConnect();
-  const session = await getServerSession(authOptions);
-  const user: User = session?.user as User;
-  if (!session || !user) {
-    return Response.json(
-      {
-        success: false,
-        message: "Not authenticated",
-      },
-      { status: 401 }
-    );
-  }
-  try {
-    const userId = new mongoose.Types.ObjectId(user._id);
-    const Alltasks = await UserModel.aggregate([
-      { $match: { _id: userId } },
-      { $unwind: "$tasks" },
-      { $sort: { "tasks.createdAt": -1 } },
-      { $group: { _id: "$_id", tasks: { $push: "$tasks" } } },
-    ]);
-    if (Alltasks.length == 0) {
-      return Response.json(
-        {
-          success: false,
-          message: "Can't find user",
-        },
-        { status: 401 }
-      );
-    }
-    return Response.json(
-      {
-        success: true,
-        message: Alltasks[0].tasks,
-      },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.log("An unexpected error occurred");
-    return Response.json(
-      {
-        success: false,
-        message: "Can't fetch messages",
-      },
-      { status: 401 }
-    );
-  }
-}
+// export async function GET(req: Request) {
+//   await dbConnect();
+//   const session = await getServerSession(authOptions);
+//   const user: User = session?.user as User;
+//   if (!session || !user) {
+//     return Response.json(
+//       {
+//         success: false,
+//         message: "Not authenticated",
+//       },
+//       { status: 401 }
+//     );
+//   }
+//   try {
+//     const userId = new mongoose.Types.ObjectId(user._id);
+//     const Alltasks = await UserModel.aggregate([
+//       { $match: { _id: userId } },
+//       { $unwind: "$tasks" },
+//       { $sort: { "tasks.createdAt": -1 } },
+//       { $group: { _id: "$_id", tasks: { $push: "$tasks" } } },
+//     ]);
+//     if (Alltasks.length == 0) {
+//       return Response.json(
+//         {
+//           success: false,
+//           message: "Can't find user",
+//         },
+//         { status: 401 }
+//       );
+//     }
+//     return Response.json(
+//       {
+//         success: true,
+//         message: Alltasks[0].tasks,
+//       },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.log("An unexpected error occurred");
+//     return Response.json(
+//       {
+//         success: false,
+//         message: "Can't fetch messages",
+//       },
+//       { status: 401 }
+//     );
+//   }
+// }
